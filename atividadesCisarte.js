cd5e0cca04fa663edc4f8f2bbf0d91cabf91539a
DeboraKass <83611317+DeboraKass@users.noreply.github.com>
2021-12-01 23:45:20 -0300

    upload

---
+++ atividadesCisarte.js
@@ -0,0 +1,163 @@
+
+$(function() {
+	$( ".descricao-um" ).click(function() {
+		$( ".descricao-texto-um" ).slideToggle( "slow" );
+
+		 // if ($(".descricao-texto-dois", ".descricao-texto-tres")[0].style.display != "none") {
+   //    		$(".descricao-texto-dois").slideToggle();
+   //   		$(".descricao-texto-tres").slideToggle();
+  	// 	 }
+		
+	});
+});
+
+$(function() {
+	$( ".descricao-dois" ).click(function() {
+			$( ".descricao-texto-dois" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-tres" ).click(function() {
+				
+		$( ".descricao-texto-tres" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-quatro" ).click(function() {
+				
+		$( ".descricao-texto-quatro" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-cinco" ).click(function() {
+				
+		$( ".descricao-texto-cinco" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-seis" ).click(function() {
+				
+		$( ".descricao-texto-seis" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-sete" ).click(function() {
+				
+		$( ".descricao-texto-sete" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-oito" ).click(function() {
+				
+		$( ".descricao-texto-oito" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-nove" ).click(function() {
+				
+		$( ".descricao-texto-nove" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-dez" ).click(function() {
+				
+		$( ".descricao-texto-dez" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-onze" ).click(function() {
+				
+		$( ".descricao-texto-onze" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-doze" ).click(function() {
+				
+		$( ".descricao-texto-doze" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-treze" ).click(function() {
+				
+		$( ".descricao-texto-treze" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-quatorze" ).click(function() {
+				
+		$( ".descricao-texto-quatorze" ).slideToggle( "slow" );
+	});
+});
+
+
+
+$(function() {
+	$( ".descricao-quinze" ).click(function() {
+				
+		$( ".descricao-texto-quinze" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-dezesseis" ).click(function() {
+				
+		$( ".descricao-texto-dezesseis" ).slideToggle( "slow" );
+	});
+});
+
+
+$(function() {
+	$( ".descricao-dezoito" ).click(function() {
+				
+		$( ".descricao-texto-dezoito" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-dezenove" ).click(function() {
+				
+		$( ".descricao-texto-dezenove" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-vinte" ).click(function() {
+				
+		$( ".descricao-texto-vinte" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-vinte-um" ).click(function() {
+				
+		$( ".descricao-texto-vinte-um" ).slideToggle( "slow" );
+	});
+});
+
+$(function() {
+	$( ".descricao-vinte-dois" ).click(function() {
+				
+		$( ".descricao-texto-vinte-dois" ).slideToggle( "slow" );
+	});
+});
---
+++ doacoesCisarte.js
@@ -0,0 +1,13 @@
+
+$(function() {
+	$( ".materiais" ).click(function() {
+			$( ".texto-doacaoMat" ).slideToggle();
+	});
+});
+
+
+$(function() {
+	$( ".dindin" ).click(function() {
+			$( ".texto-doacaoDin" ).slideToggle();
+	});
+});
