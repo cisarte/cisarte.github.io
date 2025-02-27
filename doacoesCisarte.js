08d6e6b034b950d7475de6cb5f624469764dd790
Thiago Zeba <thiagoiszeba@gmail.com>
2025-02-27 12:48:24 -0500

    Update doacoesCisarte.js

--- doacoesCisarte.js
+++ doacoesCisarte.js
@@ -1,13 +1,28 @@
+$(document).ready(function() {
+  // Hide both sections initially
+  $(".texto-doacaoDin, .texto-doacaoMat").hide();
 
-$(function() {
-	$( ".materiais" ).click(function() {
-			$( ".texto-doacaoMat" ).slideToggle();
-	});
-});
+  // Handle click for "Doar em dinheiro"
+  $(".dindin").click(function() {
+    // If the "Doar em dinheiro" section is already open, close it
+    if ($(".texto-doacaoDin").is(":visible")) {
+      $(".texto-doacaoDin").slideUp();
+    } else {
+      // Otherwise, close both sections and open the "Doar em dinheiro" section
+      $(".texto-doacaoDin, .texto-doacaoMat").slideUp();
+      $(".texto-doacaoDin").slideDown();
+    }
+  });
 
-
-$(function() {
-	$( ".dindin" ).click(function() {
-			$( ".texto-doacaoDin" ).slideToggle();
-	});
+  // Handle click for "Doar ítens físicos"
+  $(".materiais").click(function() {
+    // If the "Doar ítens físicos" section is already open, close it
+    if ($(".texto-doacaoMat").is(":visible")) {
+      $(".texto-doacaoMat").slideUp();
+    } else {
+      // Otherwise, close both sections and open the "Doar ítens físicos" section
+      $(".texto-doacaoDin, .texto-doacaoMat").slideUp();
+      $(".texto-doacaoMat").slideDown();
+    }
+  });
 });
